3c4bdefc8eafbcb2357647226685ed26dc84b422
sahib singh <ss0990100@gmail.com>
2024-04-18 01:42:49 +0530

    updated signIn controller

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -9,7 +9,13 @@ class UserController{
 
     async signIn(req,res){
         try {
-            // const response = await this.userService.
+            const response = await this.userService.signIn(req.body);
+            return res.status(201).json({
+                data:response,
+                success:true,
+                message:"successfully signIn",
+                err:{},
+            }); 
         } catch (error) {
             console.log(error);
             return res.status(500).json({
