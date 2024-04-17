3ac347fd011ebd965b5acbb07250cdab61099f69
sahib singh <ss0990100@gmail.com>
2024-04-17 23:09:20 +0530

    fixed the user service file for signIn function

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -39,7 +39,14 @@ class UserService {
 
     async signIn(data){
         try {
-            
+            const user = await this.userRepository.getUserByEmail(data.email);
+            if(!user){
+                throw "No user with such email exists";
+            }
+            if(!bcrypt.compareSync(data.password,user.password)){
+                throw "Incorrect password";
+            }
+            return true;
         } catch (error) {
             console.log(error);
             throw error;
