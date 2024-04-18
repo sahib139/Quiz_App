5c6537816d8d387c75a7726eb5b448a8a021b630
sahib singh <ss0990100@gmail.com>
2024-04-18 22:07:24 +0530

    updated QuizReponse api middleware

--- src/middleware/response-middleware.js
+++ src/middleware/response-middleware.js
@@ -1,7 +1,11 @@
 const createMiddleware = async (req,res,next)=>{
     try {
-        if(req.body.question_id === undefined)throw "question_id missing!";
-        if(req.body.selected_answer === undefined)throw "selected_answer missing!";
+        if(req.body.question_id === undefined){
+            throw "question_id missing!";
+        }
+        if(req.body.selected_answer === undefined){
+            throw "selected_answer missing!";
+        }
         req.body.user_id = req.user.id;
         next();
     } catch (error) {
@@ -18,8 +22,12 @@ const createMiddleware = async (req,res,next)=>{
 const createBulkMiddleware = async (req,res,next)=>{
     try {
         req.body.data = req.body.data.map((response)=>{
-            if(response.question_id === undefined)throw "question_id missing!";
-            if(response.selected_answer === undefined)throw "selected_answer missing!";
+            if(response.question_id === undefined){
+                throw "question_id missing!";
+            }
+            if(response.selected_answer === undefined){
+                throw "selected_answer missing!";
+            }
             response.user_id = req.user.id;
             return response;
         });
